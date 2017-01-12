affa2d7e1f6352658b8e0a873ac7266df44d529c
Nikita Veselov <qamelior@gmail.com>
2017-01-12 17:51:46 +0000

    Files hierarchy

---
+++ corridor_generator.py
@@ -0,0 +1,8 @@
+import math
+import random
+import fbx
+
+
+class corridor_generator:
+    def generate(self):
+     p = 1
--- rooms_generator_module.py
+++ rooms_generator_module.py
@@ -4,21 +4,5 @@ import fbx
 
 
 class rooms_generator:
-
-  def read_components(self):
-    importer = fbx.FbxImporter.Create(self.sdk_manager, "")    
-
-    result = importer.Initialize("scenes/components_v2.fbx", -1, self.io_settings)
-    if not result:
-      raise BaseException("could not find components file")
-    
-    #scene creation
-    self.components = fbx.FbxScene.Create(self.sdk_manager, "")
-    result = importer.Import(self.components)
-    importer.Destroy()
-
-    root = self.components.GetRootNode()
-    top_level = [root.GetChild(i) for i in range(root.GetChildCount())]
-
-    # child nodes matching this pattern are feature markup
-    feature_pattern = re.compile('(\<|\>)([^.]+)(\..*)?')
+    def swlabr(self):
+        p = 5
